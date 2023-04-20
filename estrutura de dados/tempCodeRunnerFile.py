77bf915cd931d4b40151f0bec89af43b70ac059b
pwl1m <paulorsmalheiros@gmail.com>
2023-04-20 15:52:57 -0300

    estudando pre-prova

---
+++ estrutura de dados/tempCodeRunnerFile.py
@@ -0,0 +1,22 @@
+#funcao constante
+# lista=[1,2,3,4,5]
+# lista2=[1,2,3,4,5]
+
+# #0(1) - imprimir o primeiro valor de uma lista
+# def constante(n):
+#     print(n[0])
+
+# #0(2) - imprimir os dois primeiros valores
+# def constante2(n):
+#     print(n[0])
+#     print(n[1])
+
+# constante(lista)
+# constante2(lista2)
+
+# #funcao linear - imprimindo todos os valores de uma lista
+# def linear(n):
+#     for i in n:
+#      print(i)
+
+# linear(lista)
