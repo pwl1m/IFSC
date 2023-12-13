397797f4caa1ef3bcfce54d47af60c7cdd8f8832
Paulo R S Malheiros <paulorsmalheiros@gmail.com>
2023-12-12 21:10:35 -0300

    boa ida pra tubarao professor!

---
+++ internet_2/django-1/contact/views/__init__.py
@@ -0,0 +1,7 @@
+from .contact_views import *
+from .contact_forms import *
+from .user_forms import *
+
+# engana o django para que ele não encontre o arquivo de templates do django admin 
+# e sim o arquivo de templates do projeto
+# https://stackoverflow.com/questions/5871730/overriding-django-admin-templates
